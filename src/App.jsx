8678dcf21b521f43e37ab4f16c05e64e66075cc7
imran1001 <79909513+imran1001@users.noreply.github.com>
2026-06-06 14:47:42 +0500

    Refactor App component to include new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import FAQ from './components/FAQ'; 
+import Services from './components/Services';
+import Destinations from './components/Destinations';
+import About from './components/About';
+import WhyChooseUs from './components/WhyChooseUs';
 import Testimonials from './components/Testimonials';
+import FAQ from './components/FAQ';
+import ConsultationForm from './components/ConsultationForm';
 import Footer from './components/Footer';
+import WhatsAppWidget from './components/WhatsAppWidget';
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
-      <Hero />
-      <Testimonials />
-      <FAQ />
+      <main>
+        <Hero />
+        <Services />
+        <Destinations />
+        <About />
+        <WhyChooseUs />
+        <Testimonials />
+        <FAQ />
+        <ConsultationForm />
+      </main>
       <Footer />
+      <WhatsAppWidget />
     </div>
   );
 }
